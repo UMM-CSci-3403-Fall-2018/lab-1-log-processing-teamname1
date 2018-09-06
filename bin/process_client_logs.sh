4e071cc33793d20f7cdedcf97dd00ccb05e247a7
EthanUphoff <ethan.uphoff@gmail.com>
2018-09-06 16:52:16 -0500

    Completed process_client_logs.sh
    There was a problem in one of the files which had two spaces rather than one after the month. This was added to the regex and completed this portion of the lab.

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -2,5 +2,5 @@
 directory=$(pwd)
 cd $1
 cat var/log/* > temp.txt
-sed -nr 's/([[:alnum:]]+ [[:alnum:]]+ [[:alnum:]]+):([^[:space:]]+) ([^[:space:]]+) sshd([^[:space:]]+) Failed password for([[:space:]]*[^[:space:]]*[[:space:]]*[^[:space:]]* )([[:alnum:]]+) from ([^[:space:]]+) port ([[:alnum:]]+) ssh2/\1 \6 \7/gp' < temp.txt > failed_login_data.txt
+sed -nr 's/([[:alnum:]]+)([[:space:]]*)([[:alnum:]]+ [[:alnum:]]+):([^[:space:]]+) ([^[:space:]]+) sshd([^[:space:]]+) Failed password for([[:space:]]*[^[:space:]]*[[:space:]]*[^[:space:]]* )([[:alnum:]]+) from ([^[:space:]]+) port ([[:alnum:]]+) ssh2/\1 \3 \8 \9/gp' < temp.txt > failed_login_data.txt
 rm temp.txt
