782e3c2881f099f07029e812752c34251abbaa87
EthanUphoff <ethan.uphoff@gmail.com>
2018-09-06 14:18:22 -0500

    Change the regex in process_client_logs.sh
    Removed /2 so it does not include the entire time.

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 directory=$(pwd)
 cd $1/var/log
-sed -nr 's/([[:alnum:]]+ [[:alnum:]]+ [[:alnum:]]+):([^[:space:]]+) ([^[:space:]]+) sshd([^[:space:]]+) Failed password for invalid user ([[:alnum:]]+) from ([^[:space:]]+) port ([[:alnum:]]+) ssh2/\1 \2 \5 \6/gp' < input.txt > output.txt
+sed -nr 's/([[:alnum:]]+ [[:alnum:]]+ [[:alnum:]]+):([^[:space:]]+) ([^[:space:]]+) sshd([^[:space:]]+) Failed password for invalid user ([[:alnum:]]+) from ([^[:space:]]+) port ([[:alnum:]]+) ssh2/\1 \5 \6/gp' < cat *txt > output.txt
