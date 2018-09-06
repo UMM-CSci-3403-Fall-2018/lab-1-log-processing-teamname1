0b6c961bfaf0b9c5c7079288de3f0d308f87fd8b
EthanUphoff <ethan.uphoff@gmail.com>
2018-09-06 16:22:29 -0500

    Fixed more regex problems
    The regex was not picking up anything that was not an invalid user and the issue has been resolved.

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 directory=$(pwd)
-cd $1/var/log
-sed -nr 's/([[:alnum:]]+ [[:alnum:]]+ [[:alnum:]]+):([^[:space:]]+) ([^[:space:]]+) sshd([^[:space:]]+) Failed password for invalid user ([[:alnum:]]+) from ([^[:space:]]+) port ([[:alnum:]]+) ssh2/\1 \5 \6/gp' < cat *txt > output.txt
+cd $1
+cat var/log/* > temp.txt
+sed -nr 's/([[:alnum:]]+ [[:alnum:]]+ [[:alnum:]]+):([^[:space:]]+) ([^[:space:]]+) sshd([^[:space:]]+) Failed password for([[:space:]]*[^[:space:]]*[[:space:]]*[^[:space:]]* )([[:alnum:]]+) from ([^[:space:]]+) port ([[:alnum:]]+) ssh2/\1 \6 \7/gp' < temp.txt > failed_login_data.txt
+rm temp.txt
