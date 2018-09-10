9884da395f261a806f31870700b5228c471d38b1
EthanUphoff <ethan.uphoff@gmail.com>
2018-09-10 00:16:30 -0500

    Completed create_username_dist.sh
    create_username_dist.sh does what it is supposed to but currently fails testing.
    It now properly creates username_dist.html in the format requested.

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+here=$(pwd)
 cd $1
-sed -nr "s/([[:alnum:]]+) ([[:alnum:]]+) ([[:alnum:]]+) ([[:alnum:]]+) ([^[:space:]]+)/\4/gp" < failed_login_data.txt | sort | uniq -c > temp.txt
-sed -nr "s/([[:space:]]*)([[:alnum:]]+) ([[:alnum:]]+)/data.addRow(['\3', \2])/gp" < temp.txt > test1.txt
+cat * > temp0.txt
+sed -nr "s/([[:alnum:]]+) ([[:alnum:]]+) ([[:alnum:]]+) ([[:alnum:]]+) ([^[:space:]]+)/\4/gp" < temp0.txt | sort | uniq -c > temp.txt
+sed -nr "s/([[:space:]]*)([[:alnum:]]+) ([[:alnum:]]+)/data.addRow(['\3', \2]);/gp" < temp.txt > temp1.txt
+cat temp1.txt > temp2.html
+rm temp0.txt
 rm temp.txt
+rm temp1.txt
+cat $here/html_components/username_dist_header.html temp2.html $here/html_components/username_dist_footer.html > username_dist.html
+rm temp2.html
