6d3b635517427b02aaeb34283f3ec1cf5c2cc1d5
EthanUphoff <ethan.uphoff@gmail.com>
2018-09-11 10:47:24 -0500

    Made create_username_dist.sh combine all failed login files
    create_username_dist.sh now looks into every folder in a directory and combines the failed login file in each folder to get the correct output.

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
+#storing the starting location
 here=$(pwd)
+
+#cd into the desired directory
 cd $1
-cat * > temp0.txt
+
+#this combines all of the failed_login_data.txt files into one temp file 
+cat ./*/failed_login_data.txt > temp0.txt
+
+#this sed takes all the names in temp0.txt, ignoring the rest of the data, sorts it, counts each name using uniq and puts it into temp.txt
 sed -nr "s/([[:alnum:]]+) ([[:alnum:]]+) ([[:alnum:]]+) ([[:alnum:]]+) ([^[:space:]]+)/\4/gp" < temp0.txt | sort | uniq -c > temp.txt
-sed -nr "s/([[:space:]]*)([[:alnum:]]+) ([[:alnum:]]+)/data.addRow(['\3', \2]);/gp" < temp.txt > temp1.txt
-cat temp1.txt > temp2.html
-rm temp0.txt
+
+#this sed parses the count and names from temp.txt, putting them in the desired format, and puts them in temp1.html
+sed -nr "s/([[:space:]]*)([[:alnum:]]+) ([[:alnum:]]+)/data.addRow(['\3', \2]);/gp" < temp.txt > temp1.html
+
+#combines username_dist_header.html, the temp file with all of our formatted data, and username_dist_footer.html
+cat $here/html_components/username_dist_header.html temp1.html $here/html_components/username_dist_footer.html > username_dist.html
+
+#deletes temp files
+rm temp1.html
 rm temp.txt
-rm temp1.txt
-cat $here/html_components/username_dist_header.html temp2.html $here/html_components/username_dist_footer.html > username_dist.html
-rm temp2.html
+rm temp0.txt
+
+#Note: Sed can only take files as an input. It will fail if raw text is given to it.
+#This means we have to create temp files to store text for the next sed.
