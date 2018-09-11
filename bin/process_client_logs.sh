1fe63b28d8a47778f5a06229d490772eca7bf919
EthanUphoff <ethan.uphoff@gmail.com>
2018-09-11 10:56:01 -0500

    Added comments and removed unused code
    Added comments to the process_client_logs.sh file and removed the unused directory variable.

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-directory=$(pwd)
+#cd into the specified directory
 cd $1
+
+#combines all the files in the var/log of the directory
 cat var/log/* > temp.txt
+
+#this sed parses all of logs and only takes out the logs that are for failed logins. It parses the data from these and takes out the date, the hour of day, the name, and the ip address.
 sed -nr 's/([[:alnum:]]+)([[:space:]]*)([[:alnum:]]+ [[:alnum:]]+):([^[:space:]]+) ([^[:space:]]+) sshd([^[:space:]]+) Failed password for([[:space:]]*[^[:space:]]*[[:space:]]*[^[:space:]]* )([[:alnum:]]+) from ([^[:space:]]+) port ([[:alnum:]]+) ssh2/\1 \3 \8 \9/gp' < temp.txt > failed_login_data.txt
+
+#removes temp files
 rm temp.txt
