a2a6d264ce2c8b88dc049947d6623c85c494e29f
EthanUphoff <ethan.uphoff@gmail.com>
2018-09-11 23:12:22 -0500

    Cleaned up code
    Removed unused code.

--- bin/assemble_report.sh
+++ bin/assemble_report.sh
@@ -3,6 +3,5 @@ here=$(pwd)
 cd $1
 
 cat country_dist.html hours_dist.html username_dist.html > temp1.html
-#cat $here/html_components/summary_plots_header.html temp1.html $here/html_components/summary_plots_footer.html > failed_login_summary.html
 $here/bin/wrap_contents.sh temp1.html $here/html_components/summary_plots failed_login_summary.html
 rm temp1.html
