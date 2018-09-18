f48cd5c57e7c9c69b6b2795c4cadd512ab1358e6
EthanUphoff <ethan.uphoff@gmail.com>
2018-09-18 10:09:15 -0500

    Make process_client_logs.sh use pipes
    Changed out any temp file creation in process_client_logs.sh to pipes.

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -3,10 +3,6 @@
 cd $1
 
 #combines all the files in the var/log of the directory
-cat var/log/* > temp.txt
-
+cat var/log/* |\
 #this sed parses all of logs and only takes out the logs that are for failed logins. It parses the data from these and takes out the date, the hour of day, the name, and the ip address.
-sed -nr 's/([[:alnum:]]+)([[:space:]]*)([[:alnum:]]+ [[:alnum:]]+):([^[:space:]]+) ([^[:space:]]+) sshd([^[:space:]]+) Failed password for([[:space:]]*[^[:space:]]*[[:space:]]*[^[:space:]]* )([-_[:alnum:]]+) from ([^[:space:]]+) port ([[:alnum:]]+) ssh2/\1 \3 \8 \9/gp' < temp.txt > failed_login_data.txt
-
-#removes temp files
-rm temp.txt
+sed -nr 's/([[:alnum:]]+)([[:space:]]*)([[:alnum:]]+ [[:alnum:]]+):([^[:space:]]+) ([^[:space:]]+) sshd([^[:space:]]+) Failed password for([[:space:]]*[^[:space:]]*[[:space:]]*[^[:space:]]* )([-_[:alnum:]]+) from ([^[:space:]]+) port ([[:alnum:]]+) ssh2/\1 \3 \8 \9/gp' > failed_login_data.txt
