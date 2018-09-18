635d42b94a2a4ed9c035d881261bcf2f5d40d421
EthanUphoff <ethan.uphoff@gmail.com>
2018-09-18 10:06:47 -0500

    Remove temp file creation in create_username_dist.sh
    Changed most of the temp files in create_username_dist.sh to pipes.

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -5,25 +5,18 @@ here=$(pwd)
 #cd into the desired directory
 cd $1
 
-#this combines all of the failed_login_data.txt files into one temp file 
-cat ./*/failed_login_data.txt > temp0.txt
-
-#this sed takes all the names in temp0.txt, ignoring the rest of the data, sorts it, counts each name using uniq and puts it into temp.txt
-sed -nr "s/([[:alnum:]]+) ([[:alnum:]]+) ([[:alnum:]]+) ([-_[:alnum:]]+) ([^[:space:]]+)/\4/gp" < temp0.txt | sort | uniq -c > temp.txt
-
-#this sed parses the count and names from temp.txt, putting them in the desired format, and puts them in temp1.html
-sed -nr "s/([[:space:]]*)([[:alnum:]]+) ([-_[:alnum:]]+)/data.addRow(['\3', \2]);/gp" < temp.txt > temp1.html
-
+#this combines all of the failed_login_data.txt files 
+cat ./*/failed_login_data.txt | \
+#this sed takes all the names from all the failed_login_data.txt files, ignoring the rest of the data, sorts it, counts each name using uniq
+sed -nr "s/([[:alnum:]]+) ([[:alnum:]]+) ([[:alnum:]]+) ([-_[:alnum:]]+) ([^[:space:]]+)/\4/gp" | \
+sort | uniq -c | \
+#this sed parses the count and names, putting them in the desired format, and puts them in temp.txt
+sed -nr "s/([[:space:]]*)([[:alnum:]]+) ([-_[:alnum:]]+)/data.addRow(['\3', \2]);/gp" > temp.txt
 #combines username_dist_header.html, the temp file with all of our formatted data, and username_dist_footer.html
-cat $here/html_components/username_dist_header.html temp1.html $here/html_components/username_dist_footer.html > username_dist.html
+$here/bin/wrap_contents.sh temp.txt $here/html_components/username_dist username_dist.html
 
-#deletes temp files
-rm temp1.html
+#remove temp files
 rm temp.txt
-rm temp0.txt
-
-#Note: Sed can only take files as an input. It will fail if raw text is given to it.
-#This means we have to create temp files to store text for the next sed.
 
 #Note, why we used sed instead of awk so we don't lose points: Our group has extensive knowledge of regex so we decided
 #using the more regex friendly sed would be the better choice in this situation. Sed does create excess files but allows us to do much more with regex.
